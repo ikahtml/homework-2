85d29ce728c5272948feeaeb466860b27da43788
ikahtml <100506530+ikahtml@users.noreply.github.com>
2022-05-20 17:36:41 +0400

    Add files via upload
    javascript homework N2

---
+++ script.js
@@ -0,0 +1,78 @@
+//  დავალება 1.....შექმენით ფუნქცია, რომელსაც გადავცემთ n რაოდენობის მნიშვნელობას და დააბრუნოს მხოლოდ დადებითი რიცხვების ჯამi:
+
+// function basicFunction(...numbers){
+    // for( let x of numbers)
+        // if( x > 0);
+            // let y = 0
+            // for( let y of x )
+            // if ( y += x)
+        // 
+    // 
+    // console.log(y);
+// }
+// basicFunction(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8)
+
+
+// დავალება 2.... ფუნქციის საშულებით გამოიტანეთ ყველა რიცხვის ჯამი:
+ 
+// function basicFunction(...N){
+    // let x = 0;
+    // 
+//   for (let y of N ){
+        // x += y;
+    // }
+        //  return x;
+    // }
+    // 
+    // let ravi = basicFunction(10, 50, 6, 7, 8, 11, 6, 3, 9);
+    // console.log(ravi);
+    // 
+
+//   დავალება 3  // ფუნქციამ უნდა დააბრუნოს სახელი და გვარი ადგილის გამოტოვებით 
+    // (მაგ: 'irakli tsotsoria') თუ isloggedin არის true და false თუ isloggedin ფროფერთი არის false;
+    
+    
+// let user = {
+    // firstname: 'irakli',
+    // lastname: 'tsotsoria',
+    // age: 32,
+    // isloggedin: true
+//   }
+// 
+//   function signUp(){
+    //   if(user.isloggedin == true){
+        //   console.log( user.firstname + '  ' + user.lastname);
+    //   }
+    //   if(user.isloggedin == fasle){
+        //   console.log(false)
+    //   }
+//   }
+//   let x = signUp()
+//   console.log(x)
+  
+ 
+ 
+//  დავალება  4... შექმენით ფუნქცია რომელსაც გადაეცემა ერთი პარამეტრი. 
+//  ეს პარამეტრი უნდა იყოს რიცხვების მასივი და ფუნქციამ უნდა დააბრუნოს ამ რიცხვებიდან მაქსიმალური:
+
+// 
+// function numbersArray(numbers){
+    // let maxNumber = 0;
+    // for( let item of numbers){
+        // if( item > maxNumber){
+            // maxNumber = item;
+        // }
+    // }
+    // return maxNumber;
+// }
+// console.log(numbersArray( [ 5, 10, 12, 30, 22, 3]));
+
+
+
+
+
+
+
+
+
+
